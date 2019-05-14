e26ca02a9a9cb20d89d3f38bc382a1783f6a0fb8
Kai Wang <charles.wangkai@gmail.com>
2019-05-14 20:42:48 +0800

    Graceful Chainsaw Jugglers

--- 2018/2/Graceful Chainsaw Jugglers/Solution.java
+++ 2018/2/Graceful Chainsaw Jugglers/Solution.java
@@ -1,7 +1,8 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
+	static int maxChosenNum;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
@@ -17,47 +18,32 @@ public class Solution {
 	}
 
 	static int solve(int R, int B) {
-		int[][] maxWays = buildEmptyMaxWays(R, B);
+		maxChosenNum = -1;
 
-		maxWays[R][B] = 0;
-		for (int r = 0; r <= R; r++) {
-			for (int b = 0; b <= B; b++) {
-				if (r == 0 && b == 0) {
-					continue;
-				}
+		search(R, B, -1, Integer.MAX_VALUE, 0);
 
-				int[][] addedMaxWays = buildEmptyMaxWays(R, B);
-
-				for (int i = 0; i <= R; i++) {
-					for (int j = 0; j <= B; j++) {
-						if (maxWays[i][j] >= 0 && i >= r && j >= b) {
-							addedMaxWays[i - r][j - b] = maxWays[i][j] + 1;
-						}
-					}
-				}
-
-				for (int i = 0; i <= R; i++) {
-					for (int j = 0; j <= B; j++) {
-						maxWays[i][j] = Math.max(maxWays[i][j], addedMaxWays[i][j]);
-					}
-				}
-			}
-		}
-
-		int result = -1;
-		for (int i = 0; i <= R; i++) {
-			for (int j = 0; j <= B; j++) {
-				result = Math.max(result, maxWays[i][j]);
-			}
-		}
-		return result;
+		return maxChosenNum;
 	}
 
-	static int[][] buildEmptyMaxWays(int R, int B) {
-		int[][] result = new int[R + 1][B + 1];
-		for (int i = 0; i < result.length; i++) {
-			Arrays.fill(result[i], -1);
+	static void search(int remainR, int remainB, int chosenNum, int maxB, int r) {
+		maxChosenNum = Math.max(maxChosenNum, chosenNum);
+
+		if (r * (maxChosenNum - chosenNum) > remainR) {
+			return;
 		}
-		return result;
+
+		int count = 0;
+
+		for (int b = 0; b <= maxB; b++) {
+			count++;
+			remainR -= r;
+			remainB -= b;
+
+			if (remainR < 0 || remainB < 0) {
+				break;
+			}
+
+			search(remainR, remainB, chosenNum + count, b, r + 1);
+		}
 	}
 }

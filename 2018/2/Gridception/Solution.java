738d05422c95e02391d5684b2ca95d99538fbb52
Kai Wang <charles.wangkai@gmail.com>
2019-05-15 22:40:35 +0800

    Gridception

--- 2018/2/Gridception/Solution.java
+++ 2018/2/Gridception/Solution.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Solution {
+	static final char[] COLORS = { 'B', 'W' };
 	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
 	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
 
@@ -30,26 +31,42 @@ public class Solution {
 		int R = grid.length;
 		int C = grid[0].length;
 
-		int result = 0;
-		for (int code = (1 << (R * C)) - 1; code >= 0; code--) {
-			boolean[][] pattern = decode(R, C, code);
+		int result = -1;
+		for (int upperMaxR = 0; upperMaxR < R; upperMaxR++) {
+			for (int leftMaxC = 0; leftMaxC < C; leftMaxC++) {
+				for (char upperLeftColor : COLORS) {
+					for (char upperRightColor : COLORS) {
+						for (char lowerLeftColor : COLORS) {
+							for (char lowerRightColor : COLORS) {
+								if (upperMaxR == R - 1 || leftMaxC == C - 1 || isExist(grid, upperLeftColor,
+										upperRightColor, lowerLeftColor, lowerRightColor)) {
+									boolean[][] visited = new boolean[R][C];
 
-			if (isValid(pattern) && isContain(goDeep(goDeep(grid)), grid, pattern)) {
-				result = Math.max(result, countCell(pattern));
+									for (int r = 0; r < R; r++) {
+										for (int c = 0; c < C; c++) {
+											result = Math.max(result, search(grid, upperMaxR, leftMaxC, upperLeftColor,
+													upperRightColor, lowerLeftColor, lowerRightColor, visited, r, c));
+										}
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 		return result;
 	}
 
-	static boolean isContain(char[][] dream, char[][] grid, boolean[][] pattern) {
-		int dreamR = dream.length;
-		int dreamC = dream[0].length;
-		int patternR = pattern.length;
-		int patternC = pattern[0].length;
+	static boolean isExist(char[][] grid, char upperLeftColor, char upperRightColor, char lowerLeftColor,
+			char lowerRightColor) {
+		int R = grid.length;
+		int C = grid[0].length;
 
-		for (int minR = -patternR; minR < dreamR; minR++) {
-			for (int minC = -patternC; minC < dreamC; minC++) {
-				if (isMatch(dream, grid, pattern, minR, minC)) {
+		for (int r = 0; r < R - 1; r++) {
+			for (int c = 0; c < C - 1; c++) {
+				if (grid[r][c] == upperLeftColor && grid[r][c + 1] == upperRightColor
+						&& grid[r + 1][c] == lowerLeftColor && grid[r + 1][c + 1] == lowerRightColor) {
 					return true;
 				}
 			}
@@ -58,100 +75,43 @@ public class Solution {
 		return false;
 	}
 
-	static boolean isMatch(char[][] dream, char[][] grid, boolean[][] pattern, int minR, int minC) {
-		int dreamR = dream.length;
-		int dreamC = dream[0].length;
-		int patternR = pattern.length;
-		int patternC = pattern[0].length;
+	static int search(char[][] grid, int upperMaxR, int leftMaxC, char upperLeftColor, char upperRightColor,
+			char lowerLeftColor, char lowerRightColor, boolean[][] visited, int r, int c) {
+		int R = grid.length;
+		int C = grid[0].length;
 
-		for (int i = 0; i < patternR; i++) {
-			for (int j = 0; j < patternC; j++) {
-				int r = minR + i;
-				int c = minC + j;
-
-				if (pattern[i][j] && !(r >= 0 && r < dreamR && c >= 0 && c < dreamC && grid[i][j] == dream[r][c])) {
-					return false;
-				}
-			}
+		if (visited[r][c] || grid[r][c] != findTargetColor(upperMaxR, leftMaxC, upperLeftColor, upperRightColor,
+				lowerLeftColor, lowerRightColor, r, c)) {
+			return 0;
 		}
-
-		return true;
-	}
-
-	static char[][] goDeep(char[][] dream) {
-		int row = dream.length;
-		int col = dream[0].length;
-
-		char[][] result = new char[row * 2][col * 2];
-		for (int r = 0; r < result.length; r++) {
-			for (int c = 0; c < result[0].length; c++) {
-				result[r][c] = dream[r / 2][c / 2];
-			}
-		}
-		return result;
-	}
-
-	static int countCell(boolean[][] pattern) {
-		int R = pattern.length;
-		int C = pattern[0].length;
-
-		int result = 0;
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				if (pattern[r][c]) {
-					result++;
-				}
-			}
-		}
-		return result;
-	}
-
-	static boolean[][] decode(int R, int C, int code) {
-		boolean[][] result = new boolean[R][C];
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				result[r][c] = code % 2 == 0;
-
-				code /= 2;
-			}
-		}
-		return result;
-	}
-
-	static boolean isValid(boolean[][] pattern) {
-		int R = pattern.length;
-		int C = pattern[0].length;
-
-		boolean[][] visited = new boolean[R][C];
-		boolean first = true;
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				if (pattern[r][c] && !visited[r][c]) {
-					if (first) {
-						search(pattern, visited, r, c);
-
-						first = false;
-					} else {
-						return false;
-					}
-				}
-			}
-		}
-		return true;
-	}
-
-	static void search(boolean[][] pattern, boolean[][] visited, int r, int c) {
-		int R = pattern.length;
-		int C = pattern[0].length;
-
 		visited[r][c] = true;
 
+		int result = 1;
 		for (int i = 0; i < R_OFFSETS.length; i++) {
 			int adjR = r + R_OFFSETS[i];
 			int adjC = c + C_OFFSETS[i];
 
-			if (adjR >= 0 && adjR < R && adjC >= 0 && adjC < C && pattern[adjR][adjC] && !visited[adjR][adjC]) {
-				search(pattern, visited, adjR, adjC);
+			if (adjR >= 0 && adjR < R && adjC >= 0 && adjC < C) {
+				result += search(grid, upperMaxR, leftMaxC, upperLeftColor, upperRightColor, lowerLeftColor,
+						lowerRightColor, visited, adjR, adjC);
+			}
+		}
+		return result;
+	}
+
+	static char findTargetColor(int upperMaxR, int leftMaxC, char upperLeftColor, char upperRightColor,
+			char lowerLeftColor, char lowerRightColor, int r, int c) {
+		if (r <= upperMaxR) {
+			if (c <= leftMaxC) {
+				return upperLeftColor;
+			} else {
+				return upperRightColor;
+			}
+		} else {
+			if (c <= leftMaxC) {
+				return lowerLeftColor;
+			} else {
+				return lowerRightColor;
 			}
 		}
 	}

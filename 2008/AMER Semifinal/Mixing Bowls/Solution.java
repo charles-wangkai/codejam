cea772eedb5b75647b7e30148486a34de1151642
Kai Wang <charles.wangkai@gmail.com>
2021-01-22 19:02:30 +0800

    Mixing Bowls

---
+++ 2008/AMER Semifinal/Mixing Bowls/Solution.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int C = sc.nextInt();
+    for (int tc = 1; tc <= C; ++tc) {
+      int N = sc.nextInt();
+      String recipe = null;
+      Map<String, List<String>> mixtureToIngredients = new HashMap<>();
+      for (int i = 0; i < N; ++i) {
+        String mixture = sc.next();
+        if (i == 0) {
+          recipe = mixture;
+        }
+        mixtureToIngredients.put(mixture, new ArrayList<>());
+        int M = sc.nextInt();
+        for (int j = 0; j < M; ++j) {
+          String ingredient = sc.next();
+          mixtureToIngredients.get(mixture).add(ingredient);
+        }
+      }
+
+      System.out.println(String.format("Case #%d: %d", tc, solve(recipe, mixtureToIngredients)));
+    }
+
+    sc.close();
+  }
+
+  static int solve(String recipe, Map<String, List<String>> mixtureToIngredients) {
+    Map<String, Integer> mixtureToBowlNum = new HashMap<>();
+    search(mixtureToIngredients, mixtureToBowlNum, recipe);
+
+    return mixtureToBowlNum.get(recipe);
+  }
+
+  static void search(
+      Map<String, List<String>> mixtureToIngredients,
+      Map<String, Integer> mixtureToBowlNum,
+      String mixture) {
+    List<Integer> bowlNums = new ArrayList<>();
+    for (String ingredient : mixtureToIngredients.get(mixture)) {
+      if (mixtureToIngredients.containsKey(ingredient)) {
+        search(mixtureToIngredients, mixtureToBowlNum, ingredient);
+        bowlNums.add(mixtureToBowlNum.get(ingredient));
+      }
+    }
+
+    Collections.sort(bowlNums, Comparator.reverseOrder());
+
+    mixtureToBowlNum.put(
+        mixture,
+        Math.max(
+            IntStream.range(0, bowlNums.size()).map(i -> bowlNums.get(i) + i).max().orElse(0),
+            bowlNums.size() + 1));
+  }
+}

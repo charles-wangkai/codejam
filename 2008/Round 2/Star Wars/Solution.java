0e1617eb9e8d27129c7ae06ada821dfb3a3005aa
Kai Wang <charles.wangkai@gmail.com>
2022-05-11 00:08:33 +0800

    Star Wars

--- 2008/Round 2/Star Wars/Solution.java
+++ 2008/Round 2/Star Wars/Solution.java
@@ -1,6 +1,11 @@
+// https://codingcompetitions.withgoogle.com/codejam/round/0000000000432cc0/0000000000432a80#analysis
+
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Solution {
+  static final int ITERATION_NUM = 100;
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
@@ -25,17 +30,64 @@ public class Solution {
   }
 
   static double solve(int[] x, int[] y, int[] z, int[] p) {
-    double result = 0;
-    for (int i = 0; i < x.length; ++i) {
-      for (int j = i + 1; j < x.length; ++j) {
-        result =
-            Math.max(
-                result,
-                (double) (Math.abs(x[i] - x[j]) + Math.abs(y[i] - y[j]) + Math.abs(z[i] - z[j]))
-                    / (p[i] + p[j]));
+    double lower = 0;
+    double upper = 3_000_000;
+    for (int i = 0; i < ITERATION_NUM; ++i) {
+      double middle = (lower + upper) / 2;
+      if (check(x, y, z, p, middle)) {
+        upper = middle;
+      } else {
+        lower = middle;
       }
     }
 
-    return result;
+    return (lower + upper) / 2;
+  }
+
+  static boolean check(int[] x, int[] y, int[] z, int[] p, double power) {
+    int N = x.length;
+
+    double A =
+        IntStream.range(0, N)
+            .mapToDouble(i -> x[i] + y[i] + z[i] - p[i] * power)
+            .max()
+            .getAsDouble();
+    double B =
+        IntStream.range(0, N)
+            .mapToDouble(i -> x[i] + y[i] + z[i] + p[i] * power)
+            .min()
+            .getAsDouble();
+    double C =
+        IntStream.range(0, N)
+            .mapToDouble(i -> x[i] + y[i] - z[i] - p[i] * power)
+            .max()
+            .getAsDouble();
+    double D =
+        IntStream.range(0, N)
+            .mapToDouble(i -> x[i] + y[i] - z[i] + p[i] * power)
+            .min()
+            .getAsDouble();
+    double E =
+        IntStream.range(0, N)
+            .mapToDouble(i -> x[i] - y[i] + z[i] - p[i] * power)
+            .max()
+            .getAsDouble();
+    double F =
+        IntStream.range(0, N)
+            .mapToDouble(i -> x[i] - y[i] + z[i] + p[i] * power)
+            .min()
+            .getAsDouble();
+    double G =
+        IntStream.range(0, N)
+            .mapToDouble(i -> -x[i] + y[i] + z[i] - p[i] * power)
+            .max()
+            .getAsDouble();
+    double H =
+        IntStream.range(0, N)
+            .mapToDouble(i -> -x[i] + y[i] + z[i] + p[i] * power)
+            .min()
+            .getAsDouble();
+
+    return A <= B && C <= D && E <= F && G <= H && B - G >= C + E && D + F >= A - H;
   }
 }

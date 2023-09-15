5ae0fa6e84e69f26989abe1cb678ae303f50bfa9
Kai Wang <charles.wangkai@gmail.com>
2023-09-15 23:58:33 +0800

    Apocalypse Soon

--- 2008/APAC Semifinal/Apocalypse Soon/Main.java
+++ 2008/APAC Semifinal/Apocalypse Soon/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
-public class Solution {
+public class Main {
   static final int[] R_OFFSETS = {-1, 0, 0, 1};
   static final int[] C_OFFSETS = {0, -1, 1, 0};
 

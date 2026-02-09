48c01b3ea5a751cd3daabd1cbfb21c9a20298af8
Kai Wang <charles.wangkai@gmail.com>
2026-02-09 14:29:16 +0800

    Square Math

--- 2009/Round 1B/Square Math/Main.java
+++ 2009/Round 1B/Square Math/Main.java
@@ -6,19 +6,20 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
-public class Solution {
+public class Main {
   static final int[] R_OFFSETS = {-1, 0, 1, 0};
   static final int[] C_OFFSETS = {0, 1, 0, -1};
+  static final int BASE = 128;
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
     int T = sc.nextInt();
-    for (int tc = 1; tc <= T; ++tc) {
+    for (int tc = 0; tc < T; ++tc) {
       int W = sc.nextInt();
       int Q = sc.nextInt();
       char[][] square = new char[W][W];
@@ -33,7 +34,7 @@ public class Solution {
         queries[i] = sc.nextInt();
       }
 
-      System.out.println(String.format("Case #%d:\n%s", tc, solve(square, queries)));
+      System.out.println(String.format("Case #%d:\n%s", tc + 1, solve(square, queries)));
     }
 
     sc.close();
@@ -42,48 +43,51 @@ public class Solution {
   static String solve(char[][] square, int[] queries) {
     int W = square.length;
 
-    String[] solutions = new String[queries.length];
     @SuppressWarnings("unchecked")
-    Map<Integer, Element>[][] seen = new Map[W][W];
+    Map<Integer, Element>[][] seenMaps = new Map[W][W];
     for (int r = 0; r < W; ++r) {
       for (int c = 0; c < W; ++c) {
-        seen[r][c] = new HashMap<>();
+        seenMaps[r][c] = new HashMap<>();
       }
     }
+
     List<Element> elements = new ArrayList<>();
     for (int r = 0; r < W; ++r) {
       for (int c = 0; c < W; ++c) {
         if (Character.isDigit(square[r][c])) {
-          int value = square[r][c] - '0';
-
-          elements.add(new Element(square[r][c], r, c, -1, -1, value, -1));
+          elements.add(new Element(square[r][c], r, c, -1, -1, square[r][c] - '0', -1));
         }
       }
     }
     Collections.sort(elements, Comparator.comparing(e -> e.value));
     Queue<Element> queue = new ArrayDeque<>(elements);
 
+    Map<Integer, String> queryToExpression = new HashMap<>();
+    for (int query : queries) {
+      queryToExpression.put(query, null);
+    }
+
+    int rest = queryToExpression.size();
     while (true) {
-      int prevCode = queue.peek().code;
+      int prevHash = queue.peek().hash;
       int prevValue = queue.peek().value;
 
       elements = new ArrayList<>();
-      while (!queue.isEmpty() && queue.peek().code == prevCode) {
+      while (!queue.isEmpty() && queue.peek().hash == prevHash) {
         Element head = queue.poll();
-        if (seen[head.r][head.c].containsKey(head.value)) {
-          continue;
-        }
-        seen[head.r][head.c].put(head.value, head);
+        if (!seenMaps[head.r][head.c].containsKey(head.value)) {
+          seenMaps[head.r][head.c].put(head.value, head);
 
-        for (int i = 0; i < queries.length; ++i) {
-          if (solutions[i] == null && queries[i] == head.value) {
-            solutions[i] = buildExpression(square, seen, head);
+          if (queryToExpression.containsKey(head.value)
+              && queryToExpression.get(head.value) == null) {
+            queryToExpression.put(head.value, buildExpression(square, seenMaps, head));
+            --rest;
           }
-        }
 
-        elements.add(head);
+          elements.add(head);
+        }
       }
-      if (Arrays.stream(solutions).allMatch(Objects::nonNull)) {
+      if (rest == 0) {
         break;
       }
 
@@ -97,34 +101,25 @@ public class Solution {
               int r2 = r1 + R_OFFSETS[d2];
               int c2 = c1 + C_OFFSETS[d2];
               if (r2 >= 0 && r2 < W && c2 >= 0 && c2 < W) {
-                items.add(
-                    new Item(
-                        element.r,
-                        element.c,
-                        d1,
-                        d2,
-                        String.format("%c%c", square[r1][c1], square[r2][c2])));
+                items.add(new Item(element, d1, square[r1][c1], d2, square[r2][c2] - '0'));
               }
             }
           }
         }
       }
-      Collections.sort(items, Comparator.comparing(item -> item.part));
-      for (Item item : items) {
-        int r1 = item.r + R_OFFSETS[item.d1];
-        int c1 = item.c + C_OFFSETS[item.d1];
-        int r2 = r1 + R_OFFSETS[item.d2];
-        int c2 = c1 + C_OFFSETS[item.d2];
+      Collections.sort(
+          items,
+          Comparator.<Item, Character>comparing(item -> item.operator)
+              .thenComparing(item -> item.digit));
 
-        char operator = square[r1][c1];
-        int digit = square[r2][c2] - '0';
-        int value = prevValue + (operator == '+' ? 1 : -1) * digit;
+      for (Item item : items) {
+        int value = prevValue + ((item.operator == '+') ? 1 : -1) * item.digit;
 
         queue.offer(
             new Element(
-                (prevCode * 128 + item.part.charAt(0)) * 128 + item.part.charAt(1),
-                r2,
-                c2,
+                (prevHash * BASE + item.operator) * BASE + (item.digit + '0'),
+                item.element.r + R_OFFSETS[item.d1] + R_OFFSETS[item.d2],
+                item.element.c + C_OFFSETS[item.d1] + C_OFFSETS[item.d2],
                 item.d1,
                 item.d2,
                 value,
@@ -132,12 +127,14 @@ public class Solution {
       }
     }
 
-    return String.join("\n", solutions);
+    return Arrays.stream(queries)
+        .mapToObj(queryToExpression::get)
+        .collect(Collectors.joining("\n"));
   }
 
-  static String buildExpression(char[][] square, Map<Integer, Element>[][] seen, Element head) {
+  static String buildExpression(
+      char[][] square, Map<Integer, Element>[][] seenMaps, Element element) {
     StringBuilder result = new StringBuilder();
-    Element element = head;
     while (true) {
       result.append(square[element.r][element.c]);
 
@@ -152,7 +149,7 @@ public class Solution {
       int prevR2 = prevR1 - R_OFFSETS[element.d1];
       int prevC2 = prevC1 - C_OFFSETS[element.d1];
 
-      element = seen[prevR2][prevC2].get(element.prevValue);
+      element = seenMaps[prevR2][prevC2].get(element.prevValue);
     }
     result.reverse();
 
@@ -161,23 +158,23 @@ public class Solution {
 }
 
 class Item {
-  int r;
-  int c;
+  Element element;
   int d1;
+  char operator;
   int d2;
-  String part;
+  int digit;
 
-  Item(int r, int c, int d1, int d2, String part) {
-    this.r = r;
-    this.c = c;
+  Item(Element element, int d1, char operator, int d2, int digit) {
+    this.element = element;
     this.d1 = d1;
+    this.operator = operator;
     this.d2 = d2;
-    this.part = part;
+    this.digit = digit;
   }
 }
 
 class Element {
-  int code;
+  int hash;
   int r;
   int c;
   int d1;
@@ -185,8 +182,8 @@ class Element {
   int value;
   int prevValue;
 
-  Element(int code, int r, int c, int d1, int d2, int value, int prevValue) {
-    this.code = code;
+  Element(int hash, int r, int c, int d1, int d2, int value, int prevValue) {
+    this.hash = hash;
     this.r = r;
     this.c = c;
     this.d1 = d1;

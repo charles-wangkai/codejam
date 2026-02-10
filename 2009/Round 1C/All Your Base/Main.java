abdb9df06850c5795b1f5cde49a259fee9d24ad4
Kai Wang <charles.wangkai@gmail.com>
2026-02-10 09:01:25 +0800

    All Your Base

--- 2009/Round 1C/All Your Base/Main.java
+++ 2009/Round 1C/All Your Base/Main.java
@@ -2,15 +2,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
     int T = sc.nextInt();
-    for (int tc = 1; tc <= T; ++tc) {
+    for (int tc = 0; tc < T; ++tc) {
       String message = sc.next();
 
-      System.out.println(String.format("Case #%d: %d", tc, solve(message)));
+      System.out.println(String.format("Case #%d: %d", tc + 1, solve(message)));
     }
 
     sc.close();
@@ -34,6 +34,7 @@ public class Solution {
     }
 
     int base = Math.max(2, symbolToDigit.size());
+
     long result = 0;
     for (char symbol : message.toCharArray()) {
       result = result * base + symbolToDigit.get(symbol);

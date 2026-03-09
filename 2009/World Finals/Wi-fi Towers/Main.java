eca303510c8a52e43fe33eff99eb0e50002658cb
Kai Wang <charles.wangkai@gmail.com>
2026-03-09 14:14:03 +0800

    Wi-fi Towers

--- 2009/World Finals/Wi-fi Towers/Main.java
+++ 2009/World Finals/Wi-fi Towers/Main.java
@@ -14,7 +14,7 @@ public class Main {
     Scanner sc = new Scanner(System.in);
 
     int T = sc.nextInt();
-    for (int tc = 1; tc <= T; ++tc) {
+    for (int tc = 0; tc < T; ++tc) {
       int n = sc.nextInt();
       int[] x = new int[n];
       int[] y = new int[n];
@@ -27,7 +27,7 @@ public class Main {
         s[i] = sc.nextInt();
       }
 
-      System.out.println(String.format("Case #%d: %d", tc, solve(x, y, r, s)));
+      System.out.println(String.format("Case #%d: %d", tc + 1, solve(x, y, r, s)));
     }
 
     sc.close();
@@ -36,49 +36,58 @@ public class Main {
   static int solve(int[] x, int[] y, int[] r, int[] s) {
     int n = x.length;
 
-    List<Edge> edges = new ArrayList<>();
-
-    @SuppressWarnings("unchecked")
-    List<Integer>[] edgeLists = new List[n + 2];
-    for (int i = 0; i < edgeLists.length; ++i) {
-      edgeLists[i] = new ArrayList<>();
-    }
+    MaxFlow maxFlow = new MaxFlow(n + 2);
+    int source = 0;
+    int sink = n + 1;
     for (int i = 0; i < n; ++i) {
       if (s[i] < 0) {
-        addEdges(edges, edgeLists, 0, i + 1, -s[i]);
+        maxFlow.addEdges(source, i + 1, -s[i]);
       } else {
-        addEdges(edges, edgeLists, i + 1, n + 1, s[i]);
+        maxFlow.addEdges(i + 1, sink, s[i]);
       }
     }
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < n; ++j) {
         if ((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]) <= r[i] * r[i]) {
-          addEdges(edges, edgeLists, j + 1, i + 1, INF);
+          maxFlow.addEdges(j + 1, i + 1, INF);
         }
       }
     }
 
-    return Arrays.stream(s).filter(si -> si >= 0).sum() - dinic(edges, edgeLists, 0, n + 1);
+    return Arrays.stream(s).filter(si -> si >= 0).sum() - maxFlow.dinic(source, sink);
+  }
+}
+
+class MaxFlow {
+  List<Edge> edges = new ArrayList<>();
+  List<Integer>[] edgeLists;
+
+  @SuppressWarnings("unchecked")
+  MaxFlow(int size) {
+    edgeLists = new List[size];
+    for (int i = 0; i < edgeLists.length; ++i) {
+      edgeLists[i] = new ArrayList<>();
+    }
   }
 
-  static void addEdges(List<Edge> edges, List<Integer>[] edgeLists, int u, int v, int z) {
-    edges.add(new Edge(u, v, z));
+  void addEdges(int u, int v, int cap) {
+    edges.add(new Edge(u, v, cap));
     edgeLists[u].add(edges.size() - 1);
 
     edges.add(new Edge(v, u, 0));
     edgeLists[v].add(edges.size() - 1);
   }
 
-  static int dinic(List<Edge> edges, List<Integer>[] edgeLists, int s, int t) {
+  int dinic(int s, int t) {
     int result = 0;
     while (true) {
-      int[] levels = bfs(edges, edgeLists, s, t);
+      int[] levels = bfs(s, t);
       if (levels == null) {
         break;
       }
 
       while (true) {
-        int minflow = dfs(edges, edgeLists, levels, s, t, Integer.MAX_VALUE);
+        int minflow = dfs(levels, s, t, Integer.MAX_VALUE);
         if (minflow == 0) {
           break;
         }
@@ -90,7 +99,7 @@ public class Main {
     return result;
   }
 
-  static int[] bfs(List<Edge> edges, List<Integer>[] edgeLists, int s, int t) {
+  private int[] bfs(int s, int t) {
     int[] levels = new int[edgeLists.length];
     Arrays.fill(levels, -1);
     levels[s] = 0;
@@ -116,7 +125,7 @@ public class Main {
     return null;
   }
 
-  static int dfs(List<Edge> edges, List<Integer>[] edgeLists, int[] levels, int s, int t, int low) {
+  private int dfs(int[] levels, int s, int t, int low) {
     if (s == t) {
       return low;
     }
@@ -125,7 +134,7 @@ public class Main {
     for (int e : edgeLists[s]) {
       Edge edge = edges.get(e);
       if (edge.capacity != 0 && levels[edge.to] == levels[s] + 1) {
-        int next = dfs(edges, edgeLists, levels, edge.to, t, Math.min(low - result, edge.capacity));
+        int next = dfs(levels, edge.to, t, Math.min(low - result, edge.capacity));
         edge.capacity -= next;
         edges.get(e ^ 1).capacity += next;
 
@@ -142,16 +151,16 @@ public class Main {
 
     return result;
   }
-}
 
-class Edge {
-  int from;
-  int to;
-  int capacity;
+  static class Edge {
+    int from;
+    int to;
+    int capacity;
 
-  Edge(int from, int to, int capacity) {
-    this.from = from;
-    this.to = to;
-    this.capacity = capacity;
+    Edge(int from, int to, int capacity) {
+      this.from = from;
+      this.to = to;
+      this.capacity = capacity;
+    }
   }
 }

473ec98ddd9a4356315cb73c64a51fdd5421845c
Kai Wang <charles.wangkai@gmail.com>
2026-03-09 14:39:36 +0800

    Stock Charts

--- 2009/Round 2/Stock Charts/Main.java
+++ 2009/Round 2/Stock Charts/Main.java
@@ -1,7 +1,10 @@
 // https://www.cnblogs.com/justPassBy/p/5369930.html
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
@@ -10,7 +13,7 @@ public class Main {
     Scanner sc = new Scanner(System.in);
 
     int T = sc.nextInt();
-    for (int tc = 1; tc <= T; ++tc) {
+    for (int tc = 0; tc < T; ++tc) {
       int n = sc.nextInt();
       int k = sc.nextInt();
       int[][] prices = new int[n][k];
@@ -20,7 +23,7 @@ public class Main {
         }
       }
 
-      System.out.println(String.format("Case #%d: %d", tc, solve(prices)));
+      System.out.println(String.format("Case #%d: %d", tc + 1, solve(prices)));
     }
 
     sc.close();
@@ -29,52 +32,24 @@ public class Main {
   static int solve(int[][] prices) {
     int n = prices.length;
 
-    Vertex[] leftVertices = new Vertex[n];
-    for (int i = 0; i < leftVertices.length; ++i) {
-      leftVertices[i] = new Vertex();
+    MaxFlow maxFlow = new MaxFlow(2 * n + 2);
+    int source = 0;
+    int sink = 2 * n + 1;
+    for (int i = 0; i < n; ++i) {
+      maxFlow.addEdges(source, i + 1, 1);
     }
-    Vertex[] rightVertices = new Vertex[n];
-    for (int i = 0; i < rightVertices.length; ++i) {
-      rightVertices[i] = new Vertex();
+    for (int i = 0; i < n; ++i) {
+      maxFlow.addEdges(n + 1 + i, sink, 1);
     }
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < n; ++j) {
         if (isAbove(prices[i], prices[j])) {
-          leftVertices[i].adjs.add(j);
-          rightVertices[j].adjs.add(i);
+          maxFlow.addEdges(i + 1, n + 1 + j, 1);
         }
       }
     }
 
-    int matchingCount = 0;
-    for (int i = 0; i < leftVertices.length; ++i) {
-      if (leftVertices[i].matching == -1
-          && search(leftVertices, rightVertices, new boolean[rightVertices.length], i)) {
-        ++matchingCount;
-      }
-    }
-
-    return n - matchingCount;
-  }
-
-  static boolean search(
-      Vertex[] leftVertices, Vertex[] rightVertices, boolean[] rightVisited, int leftIndex) {
-    for (int rightIndex : leftVertices[leftIndex].adjs) {
-      if (!rightVisited[rightIndex]) {
-        rightVisited[rightIndex] = true;
-
-        if (rightVertices[rightIndex].matching == -1
-            || search(
-                leftVertices, rightVertices, rightVisited, rightVertices[rightIndex].matching)) {
-          leftVertices[leftIndex].matching = rightIndex;
-          rightVertices[rightIndex].matching = leftIndex;
-
-          return true;
-        }
-      }
-    }
-
-    return false;
+    return n - maxFlow.dinic(source, sink);
   }
 
   static boolean isAbove(int[] line1, int[] line2) {
@@ -82,7 +57,109 @@ public class Main {
   }
 }
 
-class Vertex {
-  List<Integer> adjs = new ArrayList<>();
-  int matching = -1;
+class MaxFlow {
+  List<Edge> edges = new ArrayList<>();
+  List<Integer>[] edgeLists;
+
+  @SuppressWarnings("unchecked")
+  MaxFlow(int size) {
+    edgeLists = new List[size];
+    for (int i = 0; i < edgeLists.length; ++i) {
+      edgeLists[i] = new ArrayList<>();
+    }
+  }
+
+  void addEdges(int u, int v, int cap) {
+    edges.add(new Edge(u, v, cap));
+    edgeLists[u].add(edges.size() - 1);
+
+    edges.add(new Edge(v, u, 0));
+    edgeLists[v].add(edges.size() - 1);
+  }
+
+  int dinic(int s, int t) {
+    int result = 0;
+    while (true) {
+      int[] levels = bfs(s, t);
+      if (levels == null) {
+        break;
+      }
+
+      while (true) {
+        int minflow = dfs(levels, s, t, Integer.MAX_VALUE);
+        if (minflow == 0) {
+          break;
+        }
+
+        result += minflow;
+      }
+    }
+
+    return result;
+  }
+
+  private int[] bfs(int s, int t) {
+    int[] levels = new int[edgeLists.length];
+    Arrays.fill(levels, -1);
+    levels[s] = 0;
+
+    Queue<Integer> queue = new ArrayDeque<>();
+    queue.offer(s);
+
+    while (!queue.isEmpty()) {
+      int head = queue.poll();
+      if (head == t) {
+        return levels;
+      }
+
+      for (int e : edgeLists[head]) {
+        Edge edge = edges.get(e);
+        if (edge.capacity != 0 && levels[edge.to] == -1) {
+          levels[edge.to] = levels[head] + 1;
+          queue.offer(edge.to);
+        }
+      }
+    }
+
+    return null;
+  }
+
+  private int dfs(int[] levels, int s, int t, int low) {
+    if (s == t) {
+      return low;
+    }
+
+    int result = 0;
+    for (int e : edgeLists[s]) {
+      Edge edge = edges.get(e);
+      if (edge.capacity != 0 && levels[edge.to] == levels[s] + 1) {
+        int next = dfs(levels, edge.to, t, Math.min(low - result, edge.capacity));
+        edge.capacity -= next;
+        edges.get(e ^ 1).capacity += next;
+
+        result += next;
+        if (result == low) {
+          break;
+        }
+      }
+    }
+
+    if (result == 0) {
+      levels[s] = -1;
+    }
+
+    return result;
+  }
+
+  static class Edge {
+    int from;
+    int to;
+    int capacity;
+
+    Edge(int from, int to, int capacity) {
+      this.from = from;
+      this.to = to;
+      this.capacity = capacity;
+    }
+  }
 }

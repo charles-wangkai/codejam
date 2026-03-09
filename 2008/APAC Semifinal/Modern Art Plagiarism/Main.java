cbf6a53617416aeb37aaace3517c0778c2c9a6d3
Kai Wang <charles.wangkai@gmail.com>
2026-03-09 16:20:39 +0800

    Modern Art Plagiarism

--- 2008/APAC Semifinal/Modern Art Plagiarism/Main.java
+++ 2008/APAC Semifinal/Modern Art Plagiarism/Main.java
@@ -1,9 +1,12 @@
 // https://raw.githubusercontent.com/google/coding-competitions-archive/main/codejam/2008/apac_semifinal/modern_art_plagiarism/analysis.pdf
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
@@ -11,12 +14,13 @@ public class Main {
     Scanner sc = new Scanner(System.in);
 
     int C = sc.nextInt();
-    for (int tc = 1; tc <= C; ++tc) {
+    for (int tc = 0; tc < C; ++tc) {
       List<Integer>[] largeAdjLists = readTree(sc);
       List<Integer>[] smallAdjLists = readTree(sc);
 
       System.out.println(
-          String.format("Case #%d: %s", tc, solve(largeAdjLists, smallAdjLists) ? "YES" : "NO"));
+          String.format(
+              "Case #%d: %s", tc + 1, solve(largeAdjLists, smallAdjLists) ? "YES" : "NO"));
     }
 
     sc.close();
@@ -78,72 +82,24 @@ public class Main {
 
   static boolean isMatched(
       boolean[][] matched, List<Integer> smallChildList, List<Integer> largeChildList) {
-    List<Vertex> leftVertices = new ArrayList<>();
-    Map<Integer, Vertex> smallChildToVertex = new HashMap<>();
-    for (int smallChild : smallChildList) {
-      Vertex vertex = new Vertex(leftVertices.size());
-      leftVertices.add(vertex);
-      smallChildToVertex.put(smallChild, vertex);
+    MaxFlow maxFlow = new MaxFlow(smallChildList.size() + largeChildList.size() + 2);
+    int source = 0;
+    int sink = smallChildList.size() + largeChildList.size() + 1;
+    for (int i = 0; i < smallChildList.size(); ++i) {
+      maxFlow.addEdges(source, i + 1, 1);
     }
-
-    List<Vertex> rightVertices = new ArrayList<>();
-    Map<Integer, Vertex> largeChildToVertex = new HashMap<>();
-    for (int largeChild : largeChildList) {
-      Vertex vertex = new Vertex(rightVertices.size());
-      rightVertices.add(vertex);
-      largeChildToVertex.put(largeChild, vertex);
+    for (int i = 0; i < largeChildList.size(); ++i) {
+      maxFlow.addEdges(smallChildList.size() + 1 + i, sink, 1);
     }
-
-    for (int smallChild : smallChildList) {
-      for (int largeChild : largeChildList) {
-        if (matched[smallChild][largeChild]) {
-          smallChildToVertex
-              .get(smallChild)
-              .adjacents
-              .add(largeChildToVertex.get(largeChild).index);
-          largeChildToVertex
-              .get(largeChild)
-              .adjacents
-              .add(smallChildToVertex.get(smallChild).index);
+    for (int i = 0; i < smallChildList.size(); ++i) {
+      for (int j = 0; j < largeChildList.size(); ++j) {
+        if (matched[smallChildList.get(i)][largeChildList.get(j)]) {
+          maxFlow.addEdges(i + 1, smallChildList.size() + 1 + j, 1);
         }
       }
     }
 
-    int matchingCount = 0;
-    for (int i = 0; i < leftVertices.size(); ++i) {
-      if (leftVertices.get(i).matching == -1
-          && augment(leftVertices, rightVertices, new boolean[rightVertices.size()], i)) {
-        ++matchingCount;
-      }
-    }
-
-    return matchingCount == leftVertices.size();
-  }
-
-  static boolean augment(
-      List<Vertex> leftVertices,
-      List<Vertex> rightVertices,
-      boolean[] rightVisited,
-      int leftIndex) {
-    for (int rightIndex : leftVertices.get(leftIndex).adjacents) {
-      if (!rightVisited[rightIndex]) {
-        rightVisited[rightIndex] = true;
-
-        if (rightVertices.get(rightIndex).matching == -1
-            || augment(
-                leftVertices,
-                rightVertices,
-                rightVisited,
-                rightVertices.get(rightIndex).matching)) {
-          leftVertices.get(leftIndex).matching = rightIndex;
-          rightVertices.get(rightIndex).matching = leftIndex;
-
-          return true;
-        }
-      }
-    }
-
-    return false;
+    return maxFlow.dinic(source, sink) == smallChildList.size();
   }
 
   static void search(
@@ -166,12 +122,109 @@ public class Main {
   }
 }
 
-class Vertex {
-  int index;
-  List<Integer> adjacents = new ArrayList<>();
-  int matching = -1;
+class MaxFlow {
+  List<Edge> edges = new ArrayList<>();
+  List<Integer>[] edgeLists;
 
-  Vertex(int index) {
-    this.index = index;
+  @SuppressWarnings("unchecked")
+  MaxFlow(int size) {
+    edgeLists = new List[size];
+    for (int i = 0; i < edgeLists.length; ++i) {
+      edgeLists[i] = new ArrayList<>();
+    }
+  }
+
+  void addEdges(int u, int v, int cap) {
+    edges.add(new Edge(u, v, cap));
+    edgeLists[u].add(edges.size() - 1);
+
+    edges.add(new Edge(v, u, 0));
+    edgeLists[v].add(edges.size() - 1);
+  }
+
+  int dinic(int s, int t) {
+    int result = 0;
+    while (true) {
+      int[] levels = bfs(s, t);
+      if (levels == null) {
+        break;
+      }
+
+      while (true) {
+        int minflow = dfs(levels, s, t, Integer.MAX_VALUE);
+        if (minflow == 0) {
+          break;
+        }
+
+        result += minflow;
+      }
+    }
+
+    return result;
+  }
+
+  private int[] bfs(int s, int t) {
+    int[] levels = new int[edgeLists.length];
+    Arrays.fill(levels, -1);
+    levels[s] = 0;
+
+    Queue<Integer> queue = new ArrayDeque<>();
+    queue.offer(s);
+
+    while (!queue.isEmpty()) {
+      int head = queue.poll();
+      if (head == t) {
+        return levels;
+      }
+
+      for (int e : edgeLists[head]) {
+        Edge edge = edges.get(e);
+        if (edge.capacity != 0 && levels[edge.to] == -1) {
+          levels[edge.to] = levels[head] + 1;
+          queue.offer(edge.to);
+        }
+      }
+    }
+
+    return null;
+  }
+
+  private int dfs(int[] levels, int s, int t, int low) {
+    if (s == t) {
+      return low;
+    }
+
+    int result = 0;
+    for (int e : edgeLists[s]) {
+      Edge edge = edges.get(e);
+      if (edge.capacity != 0 && levels[edge.to] == levels[s] + 1) {
+        int next = dfs(levels, edge.to, t, Math.min(low - result, edge.capacity));
+        edge.capacity -= next;
+        edges.get(e ^ 1).capacity += next;
+
+        result += next;
+        if (result == low) {
+          break;
+        }
+      }
+    }
+
+    if (result == 0) {
+      levels[s] = -1;
+    }
+
+    return result;
+  }
+
+  static class Edge {
+    int from;
+    int to;
+    int capacity;
+
+    Edge(int from, int to, int capacity) {
+      this.from = from;
+      this.to = to;
+      this.capacity = capacity;
+    }
   }
 }

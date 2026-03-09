0e52e05eab44b288e51c17d151784a8f3142a161
Kai Wang <charles.wangkai@gmail.com>
2026-03-09 14:53:57 +0800

    Welcome to Code Jam

--- 2009/Qualification Round/Welcome to Code Jam/Main.java
+++ 2009/Qualification Round/Welcome to Code Jam/Main.java
@@ -1,8 +1,9 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
   static final String TARGET = "welcome to code jam";
-  static final int MODULUS = 10000;
+  static final ModInt MOD_INT = new ModInt(10000);
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -25,15 +26,48 @@ public class Main {
     for (char c : text.toCharArray()) {
       for (int length = TARGET.length(); length >= 1; --length) {
         if (TARGET.charAt(length - 1) == c) {
-          dp[length] = addMod(dp[length], dp[length - 1]);
+          dp[length] = MOD_INT.addMod(dp[length], dp[length - 1]);
         }
       }
     }
 
     return dp[dp.length - 1];
   }
+}
 
-  static int addMod(int x, int y) {
-    return Math.floorMod(x + y, MODULUS);
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return Math.floorMod(x, modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return mod(x + y);
+  }
+
+  int multiplyMod(int x, int y) {
+    return mod((long) x * y);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
   }
 }

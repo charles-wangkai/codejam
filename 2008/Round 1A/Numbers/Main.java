cc1db1f47b45b3c15e399b24e436517df35ecf03
Kai Wang <charles.wangkai@gmail.com>
2026-03-10 09:49:53 +0800

    Numbers

--- 2008/Round 1A/Numbers/Main.java
+++ 2008/Round 1A/Numbers/Main.java
@@ -1,16 +1,17 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
-  static final int MODULUS = 1000;
+  static final ModInt MOD_INT = new ModInt(1000);
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
     int T = sc.nextInt();
-    for (int tc = 1; tc <= T; ++tc) {
+    for (int tc = 0; tc < T; ++tc) {
       int n = sc.nextInt();
 
-      System.out.println(String.format("Case #%d: %s", tc, solve(n)));
+      System.out.println(String.format("Case #%d: %s", tc + 1, solve(n)));
     }
 
     sc.close();
@@ -19,17 +20,10 @@ public class Main {
   static String solve(int n) {
     return String.format(
         "%03d",
-        addMod(
-            multiplyMod(multiply(new int[] {1, 0}, pow(new int[][] {{3, 1}, {5, 3}}, n))[0], 2),
+        MOD_INT.addMod(
+            MOD_INT.multiplyMod(
+                multiply(new int[] {1, 0}, pow(new int[][] {{3, 1}, {5, 3}}, n))[0], 2),
             -1));
-  }
-
-  static int addMod(int x, int y) {
-    return Math.floorMod(x + y, MODULUS);
-  }
-
-  static int multiplyMod(int x, int y) {
-    return x * y % MODULUS;
   }
 
   static int[] multiply(int[] v, int[][] m) {
@@ -38,7 +32,7 @@ public class Main {
     int[] result = new int[size];
     for (int i = 0; i < result.length; ++i) {
       for (int j = 0; j < size; ++j) {
-        result[i] = addMod(result[i], multiplyMod(v[j], m[j][i]));
+        result[i] = MOD_INT.addMod(result[i], MOD_INT.multiplyMod(v[j], m[j][i]));
       }
     }
 
@@ -52,7 +46,7 @@ public class Main {
     for (int i = 0; i < size; ++i) {
       for (int j = 0; j < size; ++j) {
         for (int k = 0; k < size; ++k) {
-          result[i][j] = addMod(result[i][j], multiplyMod(m1[i][k], m2[k][j]));
+          result[i][j] = MOD_INT.addMod(result[i][j], MOD_INT.multiplyMod(m1[i][k], m2[k][j]));
         }
       }
     }
@@ -60,23 +54,58 @@ public class Main {
     return result;
   }
 
-  static int[][] pow(int[][] base, int exponent) {
-    int size = base.length;
+  static int[][] pow(int[][] m, long exponent) {
+    if (exponent == 0) {
+      return buildEntity(m.length);
+    }
 
+    return multiply(
+        (exponent % 2 == 1) ? m : buildEntity(m.length), pow(multiply(m, m), exponent / 2));
+  }
+
+  static int[][] buildEntity(int size) {
     int[][] result = new int[size][size];
     for (int i = 0; i < size; ++i) {
       result[i][i] = 1;
     }
 
-    while (exponent != 0) {
-      if ((exponent & 1) == 1) {
-        result = multiply(result, base);
-      }
+    return result;
+  }
+}
 
-      base = multiply(base, base);
-      exponent >>= 1;
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return Math.floorMod(x, modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return mod(x + y);
+  }
+
+  int multiplyMod(int x, int y) {
+    return mod((long) x * y);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
     }
 
-    return result;
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
   }
 }
